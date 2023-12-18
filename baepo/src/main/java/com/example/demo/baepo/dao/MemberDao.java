ced248f6475a1188718b11d65986edfad575d49c
janggihun <jgh7111@gmail.com>
2023-12-18 21:23:59 +0900

    test

---
+++ baepo/src/main/java/com/example/demo/baepo/dao/MemberDao.java
@@ -0,0 +1,12 @@
+package com.example.demo.baepo.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberDao {
+
+		
+	MemberDao memberInfo();
+	
+	
+}
---
+++ baepo/src/main/java/com/example/demo/baepo/dto/MemberDto.java
@@ -0,0 +1,19 @@
+package com.example.demo.baepo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDto {
+	
+	
+	
+	private String userId;
+	private String userPw;
+	
+	
+	
+}
