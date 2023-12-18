d2611ece9cdf62dbd6a01a520881a321660c2f02
janggihun <151517439+janggihun@users.noreply.github.com>
2023-12-18 14:02:41 +0000

    Add files via upload

---
+++ baepo/src/main/java/com/example/demo/baepo/BaepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.baepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaepoApplication.class, args);
+	}
+
+}
